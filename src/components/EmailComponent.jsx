8058d79d897d289e3df8a52ee19377c7faff6630
Praises003 <praiseabiodun15@gmail.com>
2023-11-09 23:12:01 +0100

    new updates

--- src/components/EmailComponent.jsx
+++ src/components/EmailComponent.jsx
@@ -15,9 +15,9 @@ const EmailComponent = () => {
             <div className='mb-8 w-full mx-auto'>
                                 <h1 className='text-2xl md:text-3xl font-bold text-center sm:text-lg mb-3 '>Subscribe To Our Email</h1>
 
-                                <div className="flex sm:flex-col flex-row lg:w-9/12 w-full ">
-                                <input className='  bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-sky-500 focus:bg-white focus:outline-blue-500 focus:border-sky-500 block w-full p-6' type='text' id='title' name='title' value={title} placeholder='subscribe to our mail' required onChange={(e) => setTitle(e.target.value)}/> 
-                                <button onClick={onSubmit} type='submit' className='bg-blue-900 px-7 ml-3 rounded-md sm:w-full'>Subcribe</button>
+                                <div className="flex sm:flex-col flex-col md:flex-row lg:w-10/12 w-full ">
+                                <input className='  bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-sky-500 focus:bg-white focus:outline-blue-500 focus:border-sky-500 block w-full p-6 basis-4/5' type='text' id='title' name='title' value={title} placeholder='subscribe to our mail' required onChange={(e) => setTitle(e.target.value)}/> 
+                                <button onClick={onSubmit} type='submit' className='bg-blue-900 px-7 ml-3 rounded-md sm:w-full w-full basis-2/5'>Subcribe</button>
 
                                 </div>
                                 
