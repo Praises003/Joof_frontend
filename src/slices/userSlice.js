8c591378d57aa6c0599589769709390cd860c03b
Praises003 <praiseabiodun15@gmail.com>
2023-12-12 13:20:11 +0100

    new updates

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -11,7 +11,7 @@ const initialState = {
 
 export const register = createAsyncThunk('user/register', async(user, thunkApi) => {
     try {
-        const { data } = await axios.post("/api/user", user)
+        const { data } = await axios.post("https://joof-backend.onrender.com/user", user)
 
         if (data) {
             localStorage.setItem('user', JSON.stringify(data))
