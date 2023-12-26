2a0d6272362877726cd2a3f2d74872011e494bb5
Praises003 <praiseabiodun15@gmail.com>
2023-12-26 16:21:30 +0100

    frontend fix

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -11,7 +11,7 @@ const initialState = {
 
 export const register = createAsyncThunk('user/register', async(user, thunkApi) => {
     try {
-        const { data } = await axios.post("https://joof-backend.onrender.com/api/user", user, {withCredentials: true,})
+        const { data } = await axios.post("http://localhost:5000/api/user", user, {withCredentials: true,})
 
         console.log(data)
 
