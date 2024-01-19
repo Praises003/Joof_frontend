ebba4c37e0c2b02f79f0bf93512a057ba4c85699
Praises003 <praiseabiodun15@gmail.com>
2024-01-19 12:31:35 +0100

    updates

--- src/components/UserListComponent.jsx
+++ src/components/UserListComponent.jsx
@@ -15,7 +15,7 @@ const UserListComponent = () => {
 
           </div>
 
-          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black text-lg font-semibold p-2.5 rounded-md mb-2">
+          <div style={{background: "#EFF0F2"}} className="flex justify-between items-center py-2.5 text-black    rounded-md mb-2">
             <p className="text-center">#3265</p>
             <p className="text-center lg:pl">Harry Porter</p>
             {/* <p style={{background: "#414B42", color: "#37D146", fontSize: "15.9px" }} className=" text-center rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Approved</p> */}
@@ -23,7 +23,7 @@ const UserListComponent = () => {
             <button className="bg-red-600 text-white px-4 py.0.5 rounded-2xl">Delete</button>
           </div>
 
-          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black text-lg font-semibold p-2.5 rounded-md mb-2">
+          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black  py-2.5 rounded-md mb-2">
             <p className="text-center">#3112</p>
             <p className="text-center lg:pl">Lary go</p>
             {/* <p style={{background: "#544B3A", color: "#F98732", fontSize: "15.9px"}} className=" text-center rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Pending</p> */}
@@ -33,7 +33,7 @@ const UserListComponent = () => {
             
           </div>
 
-          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black text-lg font-semibold p-2.5 rounded-md mb-3">
+          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black  py-2.5 rounded-md mb-3">
             <p className="text-center">#4232</p>
             <p className="text-center lg:pl">Sumona Gang</p>
             {/* <p style={{background: "#3A4F4F", color: "#14C8C8", fontSize: "15.9px"}} className=" text-start rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5  lg:mr-10">Paid</p> */}
@@ -43,7 +43,7 @@ const UserListComponent = () => {
             
           </div>
 
-          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black lg font-semibold p-2.5 rounded-md mb-3">
+          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black  py-2.5 rounded-md mb-3">
             <p className="text-center">#5532</p>
             <p className="text-center lg:pl">David Morph</p>
             {/* <p style={{background: "#4F3C3C", color: "#AF251C", fontSize: "15.9px"}} className=" text-start rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5  lg:mrr-10">Unpaid</p> */}
@@ -52,7 +52,7 @@ const UserListComponent = () => {
 
           </div>
 
-          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black text-lg font-semibold p-2.5 rounded-md mb-3">
+          <div style={{background: "#EFF0F2"}} className="flex justify-between text-black  py-2.5 rounded-md mb-3">
             <p className="text-center">#1932</p>
             <p className="text-center lg:pl">Willium Cany</p>
             {/* <p style={{background: "#414B42", color: "#37D146", fontSize: "15.9px" }} className=" text-center rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Approved</p>
