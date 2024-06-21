53389aa27d5ed7b5e9642523a9b3b6b00fda28cb
Praises003 <praiseabiodun15@gmail.com>
2024-06-21 12:56:44 +0200

    updates

--- src/components/EventListComponent.jsx
+++ src/components/EventListComponent.jsx
@@ -93,50 +93,9 @@ console.log(reservedTable);
 })}
 
 
-          <div style={{background: "#414141"}} className="flex justify-between text-white text-lg font-semibold px-8 py-2  rounded-md mb-2">
-           
-           <p className="text-center lg:pl">Harry Porter</p>
-           <p className="">9:00am- 5:00 pm </p>
+          
 
-           <p style={{background: "#414B42", color: "#37D146", fontSize: "15.9px" }} className=" text-center rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Approved</p>
-         </div>
-
-          <div style={{background: "#414141"}} className="flex justify-between text-white text-lg font-semibold px-8 py-2  rounded-md mb-2">
-           
-            <p className="text-center lg:pl">Harry Porter</p>
-            <p className="">9:00am- 5:00 pm </p>
-
-            <p style={{background: "#414B42", color: "#37D146", fontSize: "15.9px" }} className=" text-center rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Approved</p>
-          </div>
-
-          <div style={{background: "#414141"}} className="flex justify-between text-white text-lg font-semibold rounded-md mb-2 px-8 py-2">
-            <p className="text-center lg:pl">Lary go</p>
-            <p className="pl-8">9:00am- 5:00 pm </p>
-            <p style={{background: "#544B3A", color: "#F98732", fontSize: "15.9px"}} className=" text-center rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Pending</p>
-            
-          </div>
-
-          <div style={{background: "#414141"}} className="flex justify-between text-white text-lg font-semibold p-2.5 rounded-md mb-3">
-            <p className="text-center lg:pl">Sumona Gang</p>
-            <p className="">9:00am- 5:00 pm </p>
-            <p style={{background: "#3A4F4F", color: "#14C8C8", fontSize: "15.9px"}} className=" text-start rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5  lg:mr-10">Paid</p>
-            
-          </div>
-
-          <div style={{background: "#414141"}} className="flex justify-between text-white text-lg font-semibold p-2.5 rounded-md mb-3">
-            <p className="text-center lg:pl">David Morph</p>
-            <p className="">9:00am- 5:00 pm </p>
-            <p style={{background: "#4F3C3C", color: "#AF251C", fontSize: "15.9px"}} className=" text-start rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5  lg:mrr-10">Unpaid</p>
-                     </div>
-
-          <div style={{background: "#414141"}} className="flex justify-between text-white text-lg font-semibold p-2.5 rounded-md mb-3">
-            
-            <p className="text-center lg:pl">Willium Cany</p>
-            <p className="">9:00am- 5:00 pm </p>
-            <p style={{background: "#414B42", color: "#37D146", fontSize: "15.9px" }} className=" rounded- rounded-3xl bg-blue-900 px-2.5 py-0.5">Approved</p>
-            
-          </div>
-        
+          
 
         
         </div>
